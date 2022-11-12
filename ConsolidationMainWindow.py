ef7752a734bc080018c3108464ac674dd1704c56
herrsmitty8128 <16559135+herrsmitty8128@users.noreply.github.com>
2022-11-12 07:05:42 -0500

    updates 2022-11-12 0705

--- ConsolidationMainWindow.py
+++ ConsolidationMainWindow.py
@@ -10,6 +10,33 @@ class ConsolidationMainWindow(QtWidgets.QMainWindow, MainWindow.Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.application = application
+        self.new_data()
+        # add the data models to the table views here...
         #tableView = self.findChild(QtWidgets.QTableView, 'invoiceLineItemsTableView')
         #tableView.setModel(InvoiceLineItem.TableModel(parent=tableView))
     
+
+    @QtCore.pyqtSlot()
+    def connect_data(self):
+        try:
+            # connect the data to the table veiw data models
+            pass
+        except Exception as err:
+            QtWidgets.QMessageBox.critical(self, 'Error', str(err))
+    
+
+    @QtCore.pyqtSlot()
+    def new_data(self):
+        try:
+            self.datafile = None
+            self.data = {
+                'Entity': '',
+                'Accounts': [],
+                'Cost Centers': [],
+                'Entities': [],
+                'Trial Balance': {},
+                'Adjustments': []
+            }
+            self.connect_data()
+        except Exception as err:
+            QtWidgets.QMessageBox.critical(self, 'Error', str(err))
